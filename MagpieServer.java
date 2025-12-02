a437f7e3a7e3689999b00fa4e0a3b48b41a86d83
Pedro-Montano <montanognick@icloud.com>
2025-12-02 10:25:03 -0700

    We did stff to the server

---
+++ MagpieServer.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Date;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class MagpieServer implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException{
+    final String requestMethod = exchange.getRequestMethod();
+        String requestPath = exchange.getRequestURI().getPath();
+
+        // Log the request
+        System.out.println(new Date() + " " + requestMethod + " request for MagPie: " + requestPath);
+
+        // Respond to POST requests from the chat client
+        if ("POST".equals(requestMethod) &&"/chat".equals(requestPath)) {
+            String statement = new String(exchange.getRequestBody().readAllBytes());
+            System.out.println("User said: "+statement);
+        }
+        else {
+            // If the request is not a POST request, return a 405 Method Not Allowed error
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_METHOD, 0);
+            exchange.getResponseBody().close();
+        }
+    }
+    public static void main (String[] args) {
+        // Set the default port to 8080 and the root folder to the public folder
+        int port = 8080;
+        String rootFolder = "./public";
+        
+        
+        // Create and start a new FileServer to handle file requests
+        try {
+            final HttpServer server = FileServer.buildFileServer(port, rootFolder);
+            server.createContext("/chat", new MagpieServer());
+        
+            server.start();
+        }
+        catch (IOException e) {
+            System.err.println(new Date() + " Error starting server: " + e);
+            System.exit(1); 
+        }
+
+        System.out.println(new Date() + " Server listening at http://localhost:" + port);
+    }
+}
