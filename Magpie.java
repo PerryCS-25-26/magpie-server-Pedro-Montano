67f717c57e722724f4c4f8819150830cc46f3660
Pedro-Montano <montanognick@icloud.com>
2025-12-09 10:15:52 -0700

    more methods

--- Magpie.java
+++ Magpie.java
@@ -31,39 +31,46 @@ public class Magpie
     public String getResponse(String statement)
     {
         String response = "";
-        if (statement.indexOf("no") >= 0)
+        if (findKeyword(statement,"no") >= 0)
         {
             response = "Why so negative?";
         }
-        else if (statement.indexOf("mother") >= 0
-                 || statement.indexOf("father") >= 0
-                 || statement.indexOf("sister") >= 0
-                 || statement.indexOf("brother") >= 0)
+        else if (findKeyword(statement,"mother") >= 0
+                 || findKeyword(statement,"father") >= 0
+                 || findKeyword(statement,"sister") >= 0
+                 || findKeyword(statement,"brother") >= 0)
         {
             response = "Tell me more about your family.";
         }
-        else if (statement.indexOf("cat")>=0
-                ||statement.indexOf("dog")>=0
-    )
+        else if (findKeyword(statement,"cat")>=0
+                ||findKeyword(statement,"dog")>=0)
+    
     {
         response = "Tell me more about your pets.";
     }
-        else if (statement.indexOf("Mr. Finkelstein")>=0){
+        else if (findKeyword(statement,"Mr. Finkelstein")>=0){
             response="He sounds like a good teacher.";
         }
         else if (statement.trim().equals("")){
             response="Say something, please.";
         }
-        else if (statement.indexOf("Hey")>=0){
+        else if (findKeyword(statement,"Hey")>=0){
             response="Hello how are you";
         }
-        else if (statement.indexOf("Hello")>=0){
+        else if (findKeyword(statement,"Hello")>=0){
             response="Hi how are you";
         }
-        else if (statement.indexOf("Hi")>=0){
+        else if (findKeyword(statement,"Hi")>=0){
             response="Hey how are you";
+        }// Responses which require transformations
+        else if (findKeyword(statement, "I want to") >= 0) {
+            response = transformIWantToStatement(statement);
         }
-                
+        else if (findKeyword(statement, "you") >= 0 
+                 && findKeyword(statement, "me") > findKeyword(statement, "you"))
+        {
+            response = transformYouMeStatement(statement);
+        }       
         else
         {
             response = getRandomResponse();
@@ -185,4 +192,43 @@ public class Magpie
      {
          return findKeyword(statement, goal, 0);
      }
+         /**
+     * Take a statement with "I want to <something>." and transform it into 
+     * "What would it mean to <something>?"
+     * @param statement the user statement, assumed to contain "I want to"
+     * @return the transformed statement
+     */
+    private String transformIWantToStatement(String statement)
+    {
+        // Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+
+        // Transform the statement into a question
+        String kw = "I want to";
+        int psn = findKeyword (statement, kw);
+        String restOfStatement = statement.substring(psn + kw.length()).trim();
+        return "What would it mean to " + restOfStatement + "?";
+    }
+      /**
+     * Take a statement with "you <something> me" and transform it into 
+     * "What makes you think that I <something> you?"
+     * @param statement the user statement, assumed to contain "you" followed by "me"
+     * @return the transformed statement
+     */
+    private String transformYouMeStatement(String statement)
+    {
+        String you = "you";
+        String me = "me";
+        int psnOfYou = findKeyword (statement, you, 0);
+        int psnOfMe = findKeyword (statement, me, psnOfYou + you.length());
+
+        String restOfStatement = statement.substring(psnOfYou + you.length(), psnOfMe).trim();
+        return "What makes you think that I " + restOfStatement + " you?";
+    }
+    
 }
